cbe5a4377fe21593aaf51ce15e69d3816a09ae3b
MrPetrovs <smokik@inbox.lv>
2022-03-21 20:58:46 +0200

    Variables and constructor for Connection.java

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,4 +1,18 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Connection {
+    private final Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = (ObjectOutputStream) socket.getOutputStream();
+        this.in = (ObjectInputStream) socket.getInputStream();
+    }
 }
