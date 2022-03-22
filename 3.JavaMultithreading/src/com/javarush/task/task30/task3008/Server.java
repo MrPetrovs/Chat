c8805955451ab6bfedfcdaf64b3f2febe8cc3b32
MrPetrovs <smokik@inbox.lv>
2022-03-22 14:37:21 +0200

    Completed Server class.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static class Handler extends Thread {
+        private final Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper.writeMessage("Введите порт сервера.");
+        ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt());
+        ConsoleHelper.writeMessage("Сервер запущен!");
+
+        try {
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            serverSocket.close();
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
     }
 }
