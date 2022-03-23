1c76f1e9a411d20aefeeea4a2dc503f98b828089
MrPetrovs <smokik@inbox.lv>
2022-03-23 18:15:11 +0200

    Implemented inner class BotSocketThread
    Override void clientMainLoop(). Now it send greeting message.
    Override processIncomingMessage(String message). Now it reacts for some keywords (дата, время, год), and sends to client respond on them.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,8 +1,62 @@
 package com.javarush.task.task30.task3008.client;
 
+
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class BotClient extends Client {
     public class BotSocketThread extends SocketThread {
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+            if (!message.contains(": ")) return;
+            String userName = message.substring(0, message.indexOf(":"));
+            String messageData = message.substring(message.indexOf(":") + 2);
+            String pattern;
+
+            switch (messageData) {
+                case "дата":
+                    pattern = "d.MM.YYYY";
+                    break;
+                case "день":
+                    pattern = "d";
+                    break;
+                case "месяц":
+                    pattern = "MMMM";
+                    break;
+                case "год":
+                    pattern = "YYYY";
+                    break;
+                case "время":
+                    pattern = "H:mm:ss";
+                    break;
+                case "час":
+                    pattern = "H";
+                    break;
+                case "минуты":
+                    pattern = "m";
+                    break;
+                case "секунды":
+                    pattern = "s";
+                    break;
+                default:
+                    return;
+            }
+
+            Date currentTime = Calendar.getInstance().getTime();
+            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+            sendTextMessage("Информация для " + userName + ": " + dateFormat.format(currentTime));
+        }
     }
 
     @Override
@@ -17,7 +71,7 @@ public class BotClient extends Client {
 
     @Override
     protected String getUserName() {
-        return "date_bot_" + (int)(Math.random() * 100);
+        return "date_bot_" + (int) (Math.random() * 100);
     }
 
     public static void main(String[] args) {
